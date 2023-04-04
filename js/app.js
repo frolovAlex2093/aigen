2d034b0f5ac6b38b9b5928e7b897f504d0d13b4a
frolovAlex2093 <75167740+frolovAlex2093@users.noreply.github.com>
2023-04-04 21:22:05 +0300

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,42 +1,3 @@
-// ScrollReveal().reveal(".word-about_item", {
-//   delay: 400,
-//   duration: 400,
-//   interval: 50,
-// });
-
-// ScrollReveal().reveal(".about-item-1", {
-//   delay: 300,
-//   duration: 400,
-//   interval: 50,
-// });
-// ScrollReveal().reveal(".about-item-2", {
-//   delay: 123,
-//   duration: 400,
-//   interval: 50,
-// });
-// ScrollReveal().reveal(".about-item-3", {
-//   delay: 222,
-//   duration: 400,
-//   interval: 50,
-// });
-// ScrollReveal().reveal(".about-item-4", {
-//   delay: 265,
-//   duration: 400,
-//   interval: 50,
-// });
-// ScrollReveal().reveal(".about-item-5", {
-//   delay: 188,
-//   duration: 400,
-//   interval: 50,
-// });
-// ScrollReveal().reveal(".about-item-6", {
-//   delay: 299,
-//   duration: 400,
-//   interval: 50,
-// });
-
-
-
 const swiper = new Swiper(".swiper", {
   speed: 1000,
   spaceBetween: 100,
@@ -57,57 +18,44 @@ wow = new WOW({
 });
 wow.init();
 
-const fixedBlock = document.querySelector(".sticky");
+const fixedBlock1 = document.querySelector(".sticky_1");
+const fixedBlock2 = document.querySelector(".sticky_2");
+const fixedBlockHeader = document.querySelector(".header");
 const contentBlock = document.querySelector(".word");
 
 window.addEventListener("scroll", () => {
-  if (contentBlock.getBoundingClientRect().top <= fixedBlock.offsetHeight + document.documentElement.clientWidth * 11 / 100) {
-    fixedBlock.classList.remove("animate__animated");
-    fixedBlock.classList.remove("animate__fadeInUp");
-    fixedBlock.classList.add("animate__animated");
-    fixedBlock.classList.add("animate__fadeOutUp");
+  if (contentBlock.getBoundingClientRect().top <= fixedBlock2.offsetHeight + document.documentElement.clientWidth * 16.6 / 100) {
+    fixedBlock2.classList.remove("animate__animated");
+    fixedBlock2.classList.remove("animate__fadeInUp");
+    fixedBlock2.classList.add("animate__animated");
+    fixedBlock2.classList.add("animate__fadeOutUp");
   } else {
-    fixedBlock.classList.remove("animate__animated");
-    fixedBlock.classList.remove("animate__fadeOutUp");
-    fixedBlock.classList.add("animate__animated");
-    fixedBlock.classList.add("animate__fadeInUp");
+    fixedBlock2.classList.remove("animate__animated");
+    fixedBlock2.classList.remove("animate__fadeOutUp");
+    fixedBlock2.classList.add("animate__animated");
+    fixedBlock2.classList.add("animate__fadeInUp");
+  }
+  if (contentBlock.getBoundingClientRect().top <= fixedBlock1.offsetHeight + document.documentElement.clientWidth * 12 / 100) {
+    fixedBlock1.classList.remove("animate__animated");
+    fixedBlock1.classList.remove("animate__fadeInUp");
+    fixedBlock1.classList.add("animate__animated");
+    fixedBlock1.classList.add("animate__fadeOutUp");
+  } else {
+    fixedBlock1.classList.remove("animate__animated");
+    fixedBlock1.classList.remove("animate__fadeOutUp");
+    fixedBlock1.classList.add("animate__animated");
+    fixedBlock1.classList.add("animate__fadeInUp");
+  }
+  if (contentBlock.getBoundingClientRect().top <= fixedBlockHeader.offsetHeight + document.documentElement.clientWidth * 5 / 100) {
+    fixedBlockHeader.classList.remove("animate__animated");
+    fixedBlockHeader.classList.remove("animate__fadeInUp");
+    fixedBlockHeader.classList.add("animate__animated");
+    fixedBlockHeader.classList.add("animate__fadeOutUp");
+  } else {
+    fixedBlockHeader.classList.remove("animate__animated");
+    fixedBlockHeader.classList.remove("animate__fadeOutUp");
+    fixedBlockHeader.classList.add("animate__animated");
+    fixedBlockHeader.classList.add("animate__fadeInUp");
   }
 });
 
-
-// const blocks = document.querySelectorAll('.word-about_item');
-// const visibilityZone = window.innerHeight;
-
-// let count = 0;
-
-// function handleScroll() {
-//   blocks.forEach(block => {
-//     const blockTop = block.getBoundingClientRect().top;
-//     const blockBottom = block.getBoundingClientRect().bottom;
-
-//     if (blockBottom > 0 && blockTop < visibilityZone && !block.classList.contains('visible')) {
-//       setTimeout(() => {
-//         block.classList.add('animate__slideInUp');
-//         count++;
-
-//         if (count >= 6) {
-//           window.removeEventListener('scroll', handleScroll);
-//           shuffleBlocks();
-//         }
-//       }, 2000); // 2-second delay
-//     }
-//   });
-// }
-
-// window.addEventListener('scroll', handleScroll);
-
-// function shuffleBlocks() {
-//   for (let i = blocks.length - 1; i > 0; i--) {
-//     const j = Math.floor(Math.random() * (i + 1));
-//     [blocks[i], blocks[j]] = [blocks[j], blocks[i]];
-//   }
-
-//   blocks.forEach((block, index) => {
-//     block.style.top = (index * 120) + 'px'; // adjust the positioning as needed
-//   });
-// }
