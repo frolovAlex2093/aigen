096139ec5519b8a746a459d28e8aadcbdde0628e
frolovAlex2093 <75167740+frolovAlex2093@users.noreply.github.com>
2023-04-15 12:06:36 +0300

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -8,7 +8,7 @@ const swiper = new Swiper(".swiper", {
     delay: 2000,
   },
 });
-
+if(window.innerWidth > 935){
 const swiper2 = new Swiper(".swiper_2", {
   speed: 1000,
   spaceBetween: 100,
@@ -24,6 +24,113 @@ const swiper2 = new Swiper(".swiper_2", {
   //   delay: 2000,
   // },
 });
+}
+if(window.innerWidth <= 935){
+  const swiper2 = new Swiper(".swiper_2", {
+    speed: 1000,
+    spaceBetween: 100,
+    slidesPerView: 1.248,
+    //slidesPerGroup: 1,
+    //effect: "fade",
+    loop: true,
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+    // autoplay: {
+    //   delay: 2000,
+    // },
+  });
+}
+if(window.innerWidth <= 729){
+  const swiper2 = new Swiper(".swiper_2", {
+    speed: 1000,
+    spaceBetween: 100,
+    slidesPerView: 1.278,
+    //slidesPerGroup: 1,
+    //effect: "fade",
+    loop: true,
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+    // autoplay: {
+    //   delay: 2000,
+    // },
+  });
+}
+if(window.innerWidth <= 627){
+  const swiper2 = new Swiper(".swiper_2", {
+    speed: 1000,
+    spaceBetween: 100,
+    slidesPerView: 1.298,
+    //slidesPerGroup: 1,
+    //effect: "fade",
+    loop: true,
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+    // autoplay: {
+    //   delay: 2000,
+    // },
+  });
+}
+if(window.innerWidth <= 470){
+  const swiper2 = new Swiper(".swiper_2", {
+    speed: 1000,
+    spaceBetween: 100,
+    slidesPerView: 1.398,
+    //slidesPerGroup: 1,
+    //effect: "fade",
+    loop: true,
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+    // autoplay: {
+    //   delay: 2000,
+    // },
+  });
+}
+if(window.innerWidth <= 345){
+  const swiper2 = new Swiper(".swiper_2", {
+    speed: 1000,
+    spaceBetween: 100,
+    slidesPerView: 1.438,
+    //slidesPerGroup: 1,
+    //effect: "fade",
+    loop: true,
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+    // autoplay: {
+    //   delay: 2000,
+    // },
+  });
+}
+
+
+if (window.innerWidth <= 1080) {
+  // Remove the class from the element
+  document.querySelector(".header").classList.remove('animate__animated');
+  document.querySelector(".header").classList.remove('animate__fadeInUp');
+  document.querySelector(".about").classList.remove('sticky');
+  document.querySelector(".about").classList.remove('animate__animated');
+  document.querySelector(".about").classList.remove('animate__fadeInUp');
+  document.querySelector(".sticky_1").classList.remove('animate__animated');
+  document.querySelector(".sticky_1").classList.remove('animate__fadeInUp');
+  document.querySelector(".sticky_1").classList.remove('sticky_1');
+  document.querySelector(".sticky_2").classList.remove('animate__animated');
+  document.querySelector(".sticky_2").classList.remove('animate__fadeInUp');
+  document.querySelector(".sticky_2").classList.remove('sticky_2');
+
+
+ 
+
+
+}
 
 wow = new WOW({
   boxClass: "wow",
